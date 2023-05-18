54c2698341d99905bbdebd6e51df29fc7f5012dc
Douglas Anaya <102629422+Zaindou@users.noreply.github.com>
2023-05-18 15:07:10 -0500

    add BQ and bug fixes
    add BQ and bug fixes

--- modules/environ.py
+++ modules/environ.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 
+
+current_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 load_dotenv()
 
 
@@ -19,9 +22,12 @@ SF_SECURITY_TOKEN = os.environ.get("SF_SECURITY_TOKEN")
 
 # GOOGLE AUTH
 
-GOOGLE_APPLICATION_CREDENTIALS_PATH = os.environ.get(
-    "GOOGLE_APPLICATION_CREDENTIALS_PATH"
+GOOGLE_APPLICATION_CREDENTIALS_PATH = os.path.join(
+    current_directory, os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_PATH")
 )
+
+print(GOOGLE_APPLICATION_CREDENTIALS_PATH)
+
 PROJECT_ID = os.environ.get("PROJECT_ID")
 DATASET_ID = os.environ.get("DATASET_ID")
 TABLE_ID = os.environ.get("TABLE_ID")
